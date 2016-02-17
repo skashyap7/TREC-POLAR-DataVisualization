53f36f4e841931b232da240fd60ff68565f35fbb
skashyap7 <sumankashyap22@gmail.com>
2016-02-16 16:03:40 -0800

    Updated code for BFA and corelation

--- bfa.py
+++ bfa.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 import json
+import os
+import math
+from os import listdir
+from os.path import isfile, join
 
 # code to calculate the BFA fingerprint for a document
 
@@ -7,7 +11,7 @@ import json
 def initialize(fingerprint):
 	for i in range(0,256):
 		fingerprint[i] = 0
-	return fingerprint
+	return
 
 # Function to perform companding
 def companding(fingerprint):
@@ -29,7 +33,7 @@ def output_json(fingerprint):
 
 # Function to normalize the fingerprint
 def normalize_fingerprint(fingerprint):
-	max = 0
+	max = 1
 	for i in range(0,256):
 		if fingerprint[i] > max:
 			max = fingerprint[i]
@@ -50,19 +54,88 @@ def read_bytes(filename,fingerprint):
 		bytes_from_file = input_file.read(8192)
 		while bytes_from_file:
 			for b in bytes_from_file:
-					#print("read byte {byte}".format(byte = b))
+					#print("read byte: {byte}".format(byte = b))
 					process_byte(b,fingerprint)
 			bytes_from_file = input_file.read(8192)
 	finally:
 		input_file.close
 
+def read_directory_recur(path,filelist):
+	for f in listdir(path):
+		if isfile(join(path,f)):
+			#print(join(path,f))
+			filelist.append(join(path,f))
+		else:
+			read_directory_recur(join(path,f),filelist)
+	return
+
+def cal_corelation(fp, fingerprint):
+	co = {}
+	initialize(co)
+	sigma = 0.0375
+	for i in range(0,256):
+		x = fingerprint[i] - fp[i]
+		co[i] = math.exp(-1*(math.pow(x,2))/(2*(math.pow(sigma,2))))
+	return co
+
+def update_corelation(co,corelation,file_cnt):
+	for i in range(0,256):
+		corelation[i] = (corelation[i]*file_cnt +co[i])/(file_cnt+1)
+	return
+
+def update_fingerprint(fp, fingerprint,file_cnt):
+	for i in range(0,256):
+		fingerprint[i] = (fingerprint[i]*file_cnt + fp[i])/(file_cnt+1)
+	return
+
+def compute_avg(filelist,global_fingerprint,corelation):
+	for i in range(2,len(filelist)):
+		fp = {}
+		filename = filelist[i-1]
+		initialize(fp)
+		read_bytes(filename,fp)
+		normalize_fingerprint(fp)
+		co = cal_corelation(fp,global_fingerprint)
+		if (i == 2):
+			corelation = co
+		else:
+			update_corelation(co,corelation,i-1)
+		update_fingerprint(fp,global_fingerprint,i-1)
+
+	print(" SIGNATURE")
+	show_signature(global_fingerprint)
+	myjson = output_json(global_fingerprint)
+	print(myjson)
+	print(" CORELATION")
+	show_signature(corelation)
+	myjson = output_json(corelation)
+	print(myjson)
+	return
+	
 # Main code begins here
-fingerprint = {}
-initialize(fingerprint)
-read_bytes('index.html',fingerprint)
-normalize_fingerprint(fingerprint)
-#show_signature(fingerprint)
-rep_json = output_json(fingerprint)
-print(rep_json)
-# Perform companding
-#companding(fingerprint)
+def main():
+	filelist = []
+	path = "/home/prime/ContentDetection/TrecMimeDetection/"
+	read_directory_recur(path,filelist)
+	#print (" File List contents")
+	#print (filelist)
+
+	filename = filelist[0]
+	global_fingerprint = {}
+	corelation = {}
+	initialize(global_fingerprint)
+	initialize(corelation)
+	read_bytes(filename,global_fingerprint)
+	print(" SIGNATURE")
+	#show_signature(global_fingerprint)
+	normalize_fingerprint(global_fingerprint)
+	#rep_json = output_json(global_fingerprint)
+	compute_avg(filelist,global_fingerprint,corelation)
+
+	#print(" JSON")
+	#print(rep_json)
+	# Perform companding
+	#companding(fingerprint)
+	return
+
+main()
