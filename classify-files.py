d796cfbdd8639c4de76ec9db30b8f30113a8db89
skashyap7 <sumankashyap22@gmail.com>
2016-02-19 17:04:03 -0800

    Added a multi-threaded version to detect MIME tyep

--- classify-files.py
+++ classify-files.py
@@ -7,6 +7,39 @@ import tika
 from tika import detector
 import json
 from collections import defaultdict
+import threading
+
+# Global variables
+filelist = []
+classified_data = defaultdict(list)
+
+# Multithreaded Implementations
+class detectorThread (threading.Thread):
+	def __init__(self, threadID, st_index, end_index):
+	#,filelist,classified_data,st_index,end_index):
+		threading.Thread.__init__(self)
+		self.threadID = threadID
+		self.st_index = st_index
+		self.end_index = end_index
+	def run(self):
+		print("Starting {thread}".format(thread=self.threadID))
+		detect_threaded(self.threadID,filelist, classified_data, self.st_index, self.end_index)
+		print("Exiting {thread}".format(thread=self.threadID))
+		
+# Code to detect mime type for files and then move them to seperate 
+# directory
+def detect_threaded(threadid,filelist,mapping,start_index, end_index):
+	print("Start index = {start}, end Index = {end}".format(start = start_index, end= end_index))
+	for filename in (filelist[start_index],filelist[end_index]):
+		print("Thread {threadid} processing {file}".format(threadid=threadid,file=filename))
+		detected_mime = detector.from_file(filename)
+		#if detected_mime in (config.keys())
+		#	mapping[detected_mime].append(filename)
+		threading.Lock().acquire()
+		mapping[detected_mime].append(filename)
+		threading.Lock().release()
+	return
+
 # Recursively read directory contents and add to file list
 # Issue : Can think of moving all common utility code to 
 # different place
@@ -50,8 +83,6 @@ def main(argv):
 	config_file = ''
 	target_dir = ''
 	mapping = {}
-	filelist = []
-	classified_data = defaultdict(list)
 	try:
 		opts, args = getopt.getopt(argv,"hc:t:",["config=","target="])
 	except getopt.GetoptError:
@@ -75,6 +106,28 @@ def main(argv):
 	# Issue : Think of making this code as multi-threaded for
 	# performance
 	detect_and_move(filelist,classified_data)
+	# Calling the multi-threaded version
+	'''step = len(filelist)//5
+	thread1 = detectorThread(1, 0,step);
+	thread2 = detectorThread(2, step,2*step);
+	thread3 = detectorThread(3, 2*step+1,3*step);
+	thread4 = detectorThread(4, 3*step+1,4*step);
+	thread5 = detectorThread(5, 4*step+1,len(filelist)-1);
+	
+	thread1.start()
+	thread2.start()
+	thread3.start()
+	thread4.start()
+	thread5.start()
+	threads = []
+	threads.append(thread1)
+	threads.append(thread2)
+	threads.append(thread3)
+	threads.append(thread4)
+	threads.append(thread5)
+
+	for t in threads:
+		t.join()'''
 	json_op = json.dumps(classified_data,indent=4, sort_keys=True)
 	print(json_op)
 	return
