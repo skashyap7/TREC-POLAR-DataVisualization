15c94ff134c326ac2859eb2ab7321fb25c4fdb9f
skashyap7 <sumankashyap22@gmail.com>
2016-02-23 15:11:52 -0800

    Changes to viz

--- visualization/script-all.js
+++ visualization/script-all.js
@@ -1,4 +1,5 @@
 d3.json("viz-all.json",function(data){
+	var idx = 0;
 	for(var key in data){
 		var bardata = [];
 		for (var f in data[key])
@@ -6,16 +7,23 @@ d3.json("viz-all.json",function(data){
 			bardata.push(data[key][f]);
 		}
 		show_chart(bardata,key);
+		idx++;
 	}
 });
-//for (var i =0;i<50;i++)
-//	bardata.push(Math.round(Math.random()*30+10))
+
+function colors_google(n) {
+  /*var colors_g = ["#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6", "#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99", "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262", "#5574a6", "#3b3eac"];*/
+  var colors_g = ["#ff9900", "#109618", "#990099", "#0099c6", "#dd4477", "#66aa00", "#b82e2e","#3366cc", "#dc3912", "#6633cc"];
+  return colors_g[n % colors_g.length];
+}
+
 function show_chart(bardata,mimetype){
 var tempColor;
-var height = 400,
+var height = 200,
 	width = 900,
 	barwdith = 256,
 	baroffset = 5;
+
 var yScale = d3.scale.linear()
 			.domain([0,d3.max(bardata)])
 			.range([0,height])
@@ -23,14 +31,14 @@ var yScale = d3.scale.linear()
 var xScale = d3.scale.ordinal()
 			.domain(d3.range(0,bardata.length))
 			.rangeBands([0,width])
-function colors_google(n) {
-  var colors_g = ["#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6", "#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99", "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262", "#5574a6", "#3b3eac"];
-  return colors_g[n % colors_g.length];
-}
+
+var xAxis = d3.svg.axis()
+    .scale(xScale)
+	.orient("bottom").ticks(256);
 
 var color = d3.scale.linear()
-			.domain([0,bardata.length*0.10,bardata.length*0.20,bardata.length*0.30,bardata.length*0.40,bardata.length*0.50,bardata.length*0.60,bardata.length*0.70,bardata.length*0.80,bardata.length*0.90,bardata.length])
-			.range(["#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6", "#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99", "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262", "#5574a6", "#3b3eac"])
+			.domain([0,d3.max(bardata)])
+			.range(["#3366cc", "#3b3eac"])
 
 var tooltip = d3.select('body').append('div')
 				.style('position','absolute')
@@ -39,6 +47,9 @@ var tooltip = d3.select('body').append('div')
 				.style('opacity',0)
 
 var mychart = d3.select("#chart").append('div')
+	.append("p")
+	.text("Fig : BFA for "+mimetype)
+	.attr("text-align","center")
 	.attr('class','well')
 	.append('svg')
 	.attr('width', width)
@@ -47,14 +58,14 @@ var mychart = d3.select("#chart").append('div')
 	.selectAll('rect').data(bardata)
 	.enter().append('rect')
 		.style('fill',function(d,i){
-			return color(i)	
+			return color(xScale(i))	
 		})
 		.attr('width',xScale.rangeBand())
 		.attr('x',function(d,i){
 			return xScale(i);
 		})
 		.attr('height',0)
-		.attr('width', 2)
+		.attr('width', 3)
 		.attr('y',height)
 		.on('mouseover',function(d){
 			tooltip.transition()
@@ -72,9 +83,11 @@ var mychart = d3.select("#chart").append('div')
 				.style('opacity',1)
 				.style('fill',tempColor)
 		});
-		
-		
-		
+	mychart.append("g")
+      .attr("class", "x axis")
+	  .attr("transform", "translate(0," + height + ")")
+	  .call(xAxis);
+
 mychart.transition()
 	.attr('height',function(d){
 			return yScale(d);
