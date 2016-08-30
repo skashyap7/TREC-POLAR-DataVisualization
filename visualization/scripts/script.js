9635dbb847e712133b5ac56efffd69f6ac93365e
Suman Kashyap <sumankashyap22@gmail.com>
2016-08-29 21:28:09 -0700

    Update script.js

--- visualization/scripts/script.js
+++ visualization/scripts/script.js
@@ -11,7 +11,7 @@ var height = 400,
 var tempColor;
 
 var piedata = [];
-d3.json("../data/github-data.json",function(error,data){
+d3.json("http://skashyap7.github.io/TREC-POLAR-DataVisualization/visualization/data/github-data.json",function(error,data){
 	for ( var k in data)
 	{
 		if(data.hasOwnProperty(k)){
